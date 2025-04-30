38fe9a6d3bea6da5363674025b32be655abff5a0
Md. Tarikul Islam <mdtarikulislam2710@gmail.com>
2025-05-01 01:57:31 +0600

    added teacher_details.html and updated model url and view.py

--- jurc/models.py
+++ jurc/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+from django.db import models
+
+class Teacher(models.Model):
+  name = models.CharField(max_length=200)
+  department = models.CharField(max_length=100)
+  api_author_name = models.CharField(
+    max_length=200,
+    help_text="Exact author name format for API search"
+  )
+
+  def __str__(self):
+    return self.name
--- jurc/urls.py
+++ jurc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('author-search/', views.author_search, name='author_search'),
+    path('', views.teacher_list, name='teacher_list'),
 ]
